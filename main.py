aa9ae1c2f51e2edea65915c02eb62e4f4ca20fb7
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-03 13:44:44 +0545

    when i feel complex then i will go with small one

---
+++ main.py
@@ -0,0 +1,41 @@
+# print("Hello World")
+#
+# print("Hello" " World")
+#
+# print("""Hello
+# World
+# Hi There""")
+
+# Types of Datatypes
+# 1. Numeric data types: integer, float, complex
+# a = 5
+# print(type(a))
+
+# a = complex(3,5)
+# print(a)
+# print(type(a))
+
+# 2. String data types : strings
+string = "I Love coding as coding is easy and coding is fun"
+# print(string.lower())
+# print(type(string))
+# print(string.replace("World", "learners"))
+# print(string.swapcase())
+# print(string.capitalize())
+# print(string.count("coding"))
+# print(string.find("coding"))
+print(string.rfind("coding"))
+
+
+# String Methods
+# 2.1 islower()
+# 2.2 isupper()
+# 2.3 lower()
+# 2.4 upper()
+# 2.5 replace()
+# 2.6 swapcase()
+# 2.7 capitalize()
+# 2.8 count()
+# 2.9 find()
+# 2.10 rfind()
+# 2.11 Many More...
