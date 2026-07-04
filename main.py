7d68c622f0b257f0671601282b4861baff9dbe60
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-04 20:56:18 +0545

    Hai garmi

--- main.py
+++ main.py
@@ -118,27 +118,36 @@ from xml.dom.minidom import Element
 # Comments
 # Program to display the Fibonacci sequence up to n-th term
 
-nterms = int(input("How many terms? "))
+# nterms = int(input("How many terms? "))
 
 # first two terms
-n1, n2 = 0, 1
-count = 0
+# n1, n2 = 0, 1
+# count = 0
 
 # check if the number of terms is valid
-if nterms <= 0:
-    print("Please enter a positive integer")
+# if nterms <= 0:
+#     print("Please enter a positive integer")
 
 # if there is only one term, return n1
-elif nterms == 1:
-    print("Fibonacci sequence upto", nterms, ":")
-    print(n1)
+# elif nterms == 1:
+#     print("Fibonacci sequence upto", nterms, ":")
+#     print(n1)
 # generate fibonacci sequence
-else:
-    print("Fibonacci sequence:")
-    while count < nterms:
-        print(n1, end=" ")
-        nth = n1 + n2
-        # update values
-        n1 = n2
-        n2 = nth
-        count += 1
+# else:
+#     print("Fibonacci sequence:")
+#     while count < nterms:
+#         print(n1, end=" ")
+#         nth = n1 + n2
+#         # update values
+#         n1 = n2
+#         n2 = nth
+#         count += 1
+
+# Arithmetic Operations
+# 1.1 Addition                  +
+# 1.2 Subtraction               -
+# 1.3 Multiplication            *
+# 1.4 Division                  /
+# 1.5 Modulus                   %
+# 1.6 Exponentiation            **
+# 1.7 Floor division             //
