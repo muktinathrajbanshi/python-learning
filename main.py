a0da061f4bb7e29fdd67f1161da6c36fbdca26e1
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-10 22:11:19 +0545

    wow that's great job muktinath

--- main.py
+++ main.py
@@ -403,13 +403,29 @@ from xml.sax.handler import DTDHandler
 #     else:
 #         print(i)
 # 7.1.3 Pass
-for i in range(10):
-    if i==5:
-        pass
-    else:
-        print(i)
+# for i in range(10):
+#     if i==5:
+#         pass
+#     else:
+#         print(i)
 
+# 1-minute Exercise
+# Print the Pattern by taking User Input
+# Sample Input:
+#          5
+# Sample Output:
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
 
+n = int(input("Enter any number: "))
+
+for i in range(n):
+    for j in range(1, n-i+1):
+        print(j, end=" ")
+    print("")
 
 
 
